6bb88b5f95e2dbf71e4f24374d206629e013e66f
kamat007 <53607917+kamat007@users.noreply.github.com>
2022-05-13 19:42:34 +0530

    Update dekh.

--- CrudOperationUsingSpringBoot/src/main/java/com/boot/application/repositoryinterface/EmployeeRepository.java
+++ CrudOperationUsingSpringBoot/src/main/java/com/boot/application/repositoryinterface/EmployeeRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.boot.application.entity.Employee;
 
-////without annotation how can container get the object 
+////without annotation how can container get the object   <--- haath dho kl iske piiche pad gya hai tu.🤣
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
